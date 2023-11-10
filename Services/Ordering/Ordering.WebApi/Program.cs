3db9260f15eacafa8d141ec29c588be7a670f4b4
sawek111@gmail <sawek111@gmail.com>
2023-11-10 14:44:15 +0100

    missing

---
+++ Services/Ordering/Ordering.WebApi/Program.cs
@@ -0,0 +1,45 @@
+using Ordering.Application;
+using Ordering.Domain;
+using Ordering.Domain.Email;
+using Ordering.Domain.Orders;
+using Ordering.Infrastructure;
+using Ordering.Infrastructure.Peristence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddApplication();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IEmailService, EmailService>();
+builder.Services.AddControllers();
+
+
+// TODO - Add automapper when OrderRepository done
+// builder.Services.AddMapster(typeof(OrderRepository).Assembly);
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
